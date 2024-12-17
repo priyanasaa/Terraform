ca348d1fe3f31809db287ec47d753146a76dd8b4
priyanasaa <priyanasaa@gmail.com>
2024-12-17 13:24:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0e2c8caa4b6378d8c"
   instance_type = "t2.micro"
-  key_name      = "Priyan"
+  key_name      = "Priyankey"
   count         = 3
 
   tags = {
